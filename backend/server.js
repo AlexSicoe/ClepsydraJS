9f11efc82b5c61f512e61773e57953ef7f44446f
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2018-11-05 17:56:47 +0200

    fix post stages and refactor err messages

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,13 @@ User.hasMany(Task, { as: 'reportedTask' })
 const Reporter = Task.belongsTo(User, { as: 'reporter' })
 
 
+const errMsgUser = 'cannot find user'
+const errMsgProject = 'cannot find project'
+const errMsgUserProject = 'cannot find userProject'
+const errMsgTask = 'cannot find task'
+const errMsgSprint = 'cannot find sprint'
+const errMsgStage = 'cannot find stage'
+
 
 router.get('/', (req, res) => {
   res.json({ message: 'Welcome to our wonderful REST API !!!' })
@@ -70,8 +77,10 @@ router.post('/login', async (req, res, next) => {
   }
   try {
     let user = await User.findOne(whereDetailsMatch)
-    if (!user)
+    if (!user) {
       res.status(401).send('mail and password do not match')
+      throw new Error('mail and password do not match')
+    }
     res.status(200).json(user)
   } catch (err) {
     next(err)
@@ -90,8 +99,10 @@ router.get('/users', async (req, res, next) => {
 router.get('/users/:uid', async (req, res, next) => {
   try {
     let user = await User.findById(req.params.uid, { include: [Project] })
-    if (!user)
-      res.status(404).send('user not found')
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     res.status(200).json(user)
   } catch (err) {
     next(err)
@@ -101,8 +112,10 @@ router.get('/users/:uid', async (req, res, next) => {
 router.put('/users/:uid', async (req, res, next) => {
   try {
     let user = await User.findById(req.params.uid)
-    if (!user)
-      res.status(404).send('cannot find user')
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     await user.update(req.body)
     res.status(200).send('updated user')
   } catch (err) {
@@ -115,8 +128,10 @@ router.put('/users/:uid', async (req, res, next) => {
 router.delete('/users/:uid', async (req, res, next) => {
   try {
     let user = await User.findById(req.params.uid)
-    if (!user)
-      res.status(404).send('cannot find user')
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     await user.destroy()
     res.status(200).send('removed user')
   } catch (err) {
@@ -128,8 +143,10 @@ router.get('/users/:uid/projects', async (req, res, next) => {
   //get projects from a certain user
   try {
     let user = await User.findById(req.params.uid)
-    if (!user)
-      res.status(404).send('cannot find user')
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     let projects = await user.getProjects() //TODO include role?
     res.status(200).json(projects)
   }
@@ -146,8 +163,10 @@ router.post('/users/:uid/projects', async (req, res, next) => {
 
   try {
     let user = await User.findById(req.params.uid)
-    if (!user)
-      res.status(404).send('cannot find user')
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     let project = await Project.create(req.body)
     await user.addProject(project, { through })
     res.status(201).send('created project')
@@ -169,8 +188,10 @@ router.get('/projects', async (req, res, next) => {
 router.get('/projects/:pid', async (req, res, next) => {
   try {
     let project = await Project.findById(req.params.pid, { include: [User] })
-    if (!project)
-      res.status(404).send('project not found')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     res.status(200).json(project)
   } catch (err) {
     next(err)
@@ -180,8 +201,10 @@ router.get('/projects/:pid', async (req, res, next) => {
 router.put('/projects/:pid', async (req, res, next) => {
   try {
     let project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     await project.update(req.body)
 
     res.status(200).send('updated project')
@@ -193,8 +216,10 @@ router.put('/projects/:pid', async (req, res, next) => {
 router.delete('/projects/:pid', async (req, res, next) => {
   try {
     let project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     await project.destroy()
     res.status(200).send('removed project')
   } catch (err) {
@@ -205,8 +230,10 @@ router.delete('/projects/:pid', async (req, res, next) => {
 router.get('/projects/:pid/users', async (req, res, next) => {
   try {
     let project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     let users = await project.getUsers()
     res.status(200).json(users)
   } catch (err) {
@@ -224,12 +251,18 @@ router.post('/projects/:pid/users/:uid', async (req, res, next) => {
     const findProject = Project.findById(req.params.pid)
     const findUser = User.findById(req.params.uid)
     const [project, user] = await Promise.all([findProject, findUser])
-    if (!project)
-      res.status(404).send('cannot find project')
-    if (!user)
-      res.status(404).send('cannot find user')
-    if (await project.hasUser(user))
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
+    if (await project.hasUser(user)) {
       res.status(404).send('user already belongs to project')
+      throw new Error('user already belongs to project')
+    }
     await project.addUser(user, { through })
     res.status(200).send('added user to project')
   } catch (err) {
@@ -249,8 +282,10 @@ router.put('/projects/:pid/users/:uid', async (req, res, next) => {
 
   try {
     let through = await UserProject.findOne(whereDetailsMatch)
-    if (!through)
-      res.status(404).send('cannot find userProject')
+    if (!through) {
+      res.status(404).send(errMsgUserProject)
+      throw new Error(errMsgUserProject)
+    }
     await through.update(req.body)
     res.status(200).send('userProject updated')
   } catch (err) {
@@ -264,10 +299,14 @@ router.delete('/projects/:pid/users/:uid', async (req, res, next) => {
     const findProject = Project.findById(req.params.pid)
     const findUser = User.findById(req.params.uid)
     const [project, user] = await Promise.all([findProject, findUser])
-    if (!project)
-      res.status(404).send('cannot find project')
-    if (!user)
-      res.status(404).send('cannot find user')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
+    if (!user) {
+      res.status(404).send(errMsgUser)
+      throw new Error(errMsgUser)
+    }
     if (!await project.hasUser(user))
       res.status(404).send('project doesn\'t have said user')
     await project.removeUser(user)
@@ -280,8 +319,10 @@ router.delete('/projects/:pid/users/:uid', async (req, res, next) => {
 router.get('/projects/:pid/sprints', async (req, res, next) => {
   try {
     const project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     const sprints = await project.getSprints()
     res.status(200).json(sprints)
   } catch (err) {
@@ -292,8 +333,10 @@ router.get('/projects/:pid/sprints', async (req, res, next) => {
 router.post('/projects/:pid/sprints', async (req, res, next) => {
   try {
     const project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     const sprint = await Sprint.create(req.body)
     await project.addSprint(sprint)
     res.status(201).send('sprint created')
@@ -305,8 +348,10 @@ router.post('/projects/:pid/sprints', async (req, res, next) => {
 router.put('/sprints/:sid', async (req, res, next) => {
   try {
     const sprint = await Sprint.findById(req.params.sid)
-    if (!sprint)
-      res.status(404).send('cannot find sprint')
+    if (!sprint) {
+      res.status(404).send(errMsgSprint)
+      throw new Error(errMsgSprint)
+    }
     await sprint.update(req.body)
     res.status(200).send('sprint updated')
   } catch (err) {
@@ -318,8 +363,10 @@ router.delete('/sprints/:sid', async (req, res, next) => {
   //TODO remove from project with all its stages, atomically
   try {
     const sprint = await Sprint.findById(req.params.sid)
-    if (!sprint)
-      res.status(404).send('cannot find sprint')
+    if (!sprint) {
+      res.status(404).send(errMsgSprint)
+      throw new Error(errMsgSprint)
+    }
     await sprint.destroy()
     res.status(200).send('sprint removed')
   } catch (err) {
@@ -336,8 +383,10 @@ router.get('/sprints/:sid/stages', async (req, res, next) => {
 
   try {
     const sprint = await Sprint.findById(req.params.sid)
-    if (!sprint)
-      res.status(404).send('cannot find sprint')
+    if (!sprint) {
+      res.status(404).send(errMsgSprint)
+      throw new Error(errMsgSprint)
+    }
     const stages = await sprint.getStages(withOptions)
     res.status(200).json(stages)
   } catch (err) {
@@ -346,13 +395,26 @@ router.get('/sprints/:sid/stages', async (req, res, next) => {
 })
 
 router.post('/sprints/:sid/stages', async (req, res, next) => {
+  const options = {
+    where: {
+      sprint_id: { [Op.eq]: req.params.sid }
+    }
+  }
+
   try {
-    const sprint = await Sprint.findById(req.params.sid)
-    if (!sprint)
-      res.status(404).send('cannot find sprint')
+    const findSprint = Sprint.findById(req.params.sid)
+    const findNr = Stage.max('position', options)
+    let [sprint, nr] = await Promise.all([findSprint, findNr])
+    if (!sprint) {
+      res.status(404).send(errMsgSprint)
+      throw new Error(errMsgSprint)
+    }
+    if (!nr)
+      nr = 0
+
+    req.body.position = ++nr
     const stage = await Stage.create(req.body)
-    let nr = await sprint.countStages()
-    await stage.update({ position: ++nr })
+
     sprint.addStage(stage)
     res.status(200).send('stage created')
   } catch (err) {
@@ -363,9 +425,11 @@ router.post('/sprints/:sid/stages', async (req, res, next) => {
 router.put('/stages/:stid', async (req, res, next) => {
   try {
     const stage = await Stage.findById(req.params.stid)
-    if (!stage)
-      res.status(404).send('cannot find stage')
-    stage.update(req.body)
+    if (!stage) {
+      res.status(404).send(errMsgStage)
+      throw new Error(errMsgStage)
+    }
+    await stage.update(req.body)
     res.status(200).send('updated stage')
   } catch (err) {
     next(err)
@@ -375,8 +439,10 @@ router.put('/stages/:stid', async (req, res, next) => {
 router.delete('/stages/:stid', async (req, res, next) => {
   try {
     const stage = await Stage.findById(req.params.stid)
-    if (!stage)
-      res.status(404).send('cannot find stage')
+    if (!stage) {
+      res.status(404).send(errMsgStage)
+      throw new Error(errMsgStage)
+    }
     stage.destroy()
     res.status(200).send('removed stage')
   } catch (err) {
@@ -390,10 +456,10 @@ router.patch('/stages/:stid1/:stid2', async (req, res, next) => {
     const findStage1 = Stage.findById(req.params.stid1)
     const findStage2 = Stage.findById(req.params.stid2)
     const [stage1, stage2] = await Promise.all([findStage1, findStage2])
-    if (!stage1)
-      res.status(404).send('cannot find stage1')
-    if (!stage2)
-      res.status(404).send('cannot find stage2')
+    if (!(stage1 && stage2)) {
+      res.status(404).send(errMsgStage)
+      throw new Error(errMsgStage)
+    }
     let pos1 = stage1.position
     let pos2 = stage2.position
       ;[pos1, pos2] = [pos2, pos1]
@@ -409,8 +475,10 @@ router.patch('/stages/:stid1/:stid2', async (req, res, next) => {
 router.get('/projects/:pid/tasks', async (req, res, next) => {
   try {
     const project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     const tasks = await project.getTasks()
     res.status(200).json(tasks)
   } catch (err) {
@@ -421,8 +489,10 @@ router.get('/projects/:pid/tasks', async (req, res, next) => {
 router.post('/projects/:pid/tasks', async (req, res, next) => {
   try {
     const project = await Project.findById(req.params.pid)
-    if (!project)
-      res.status(404).send('cannot find project')
+    if (!project) {
+      res.status(404).send(errMsgProject)
+      throw new Error(errMsgProject)
+    }
     let task = await Task.create(req.body)
     await project.addTask(task)
     res.status(201).send('task created')
@@ -431,11 +501,15 @@ router.post('/projects/:pid/tasks', async (req, res, next) => {
   }
 })
 
+
 router.put('/tasks/:tid', async (req, res, next) => {
   try {
     const task = await Task.findById(req.params.tid)
-    if (!task)
-      res.status(404).send('cannot find task')
+    if (!task) {
+      res.status(404).send(errMsgTask)
+      throw new Error(errMsgTask)
+    }
+
     await task.update(req.body)
     res.status(200).send('task updated')
   } catch (err) {
@@ -446,8 +520,10 @@ router.put('/tasks/:tid', async (req, res, next) => {
 router.delete('/tasks/:tid', async (req, res, next) => {
   try {
     const task = await Task.findById(req.params.tid)
-    if (!task)
-      res.status(404).send('cannot find task')
+    if (!task) {
+      res.status(404).send(errMsgTask)
+      throw new Error(errMsgTask)
+    }
     await task.destroy()
     res.status(200).send('task removed')
   } catch (err) {
@@ -460,10 +536,14 @@ router.post('sprint/:sid/tasks/:tid', async (req, res, next) => {
     const findSprint = Sprint.findById(req.params.sid)
     const findTask = Task.findById(req.params.tid)
     const [sprint, task] = await Promise.all([findSprint, findTask])
-    if (!sprint)
-      res.status(404).send('cannot find sprint')
-    if (!task)
-      res.status(404).send('cannot find task')
+    if (!sprint) {
+      res.status(404).send(errMsgSprint)
+      throw new Error(errMsgSprint)
+    }
+    if (!task) {
+      res.status(404).send(errMsgTask)
+      throw new Error(errMsgTask)
+    }
     //TODO if sprint doesn't have said task in any of its stages
     //TODO add into initial stage
   } catch (err) {
