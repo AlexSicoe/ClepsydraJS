bbd36d011df62bda0eb0e333333032c02c63c865
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2018-11-16 20:51:04 +0200

    setup redux

--- frontend/src/reducers/user-reducer.js
+++ frontend/src/reducers/user-reducer.js
@@ -2,6 +2,7 @@ const INITIAL_STATE = {
   username: null,
   password: null,
   email: null,
+  error: null,
   fetching: false,
   fetched: false,
 }
@@ -9,11 +10,43 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_USERNAME':
-      return { ...state, username: action.payload }
+      return {
+        ...state,
+        username: action.payload
+      }
     case 'SET_PASSWORD':
-      return { ...state, password: action.password }
+      return {
+        ...state,
+        password: action.payload
+      }
     case 'SET_EMAIL':
-      return { ...state, email: action.email }
+      return {
+        ...state,
+        email: action.payload
+      }
+    case 'GET_USER_PENDING':
+      return {
+        ...state,
+        error: null,
+        fetching: true,
+        fetched: false
+      }
+    case 'GET_USER_FULLFILLED':
+      return {
+        ...state,
+        error: null,
+        fetching: false,
+        fetched: true,
+        user: action.payload.data
+      }
+    case 'GET_USER_REJECTED':
+      return {
+        ...state,
+        error: action.payload,
+        fetching: false,
+        fetched: false,
+        user: null
+      }
     default:
       return state
   }
