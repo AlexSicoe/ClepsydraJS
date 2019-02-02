0e14ae4de63efc656920808c1159a8fc0972a41b
Sicoe Horia-Alexandru <alexisicoe@gmail.com>
2019-02-02 19:02:22 +0200

    update mock view

--- frontend/src/components/public/Area51.tsx
+++ frontend/src/components/public/Area51.tsx
@@ -2,29 +2,65 @@ import React, { Component } from 'react'
 // @ts-ignore
 import Board from 'react-trello'
 
-const getBoard = () => {
-  const data = {
-    lanes: [
+const data = {
+  lanes: [
+    {
+      id: 'lane1',
+      title: 'Planned Tasks',
+      label: '2/2',
+      cards: [
+        {
+          id: 'Card1',
+          title: 'Write Blog',
+          description: 'Can AI make memes',
+          label: '30 mins'
+        },
+        {
+          id: 'Card2',
+          title: 'Pay Rent',
+          description: 'Transfer via NEFT',
+          label: '5 mins',
+          metadata: { sha: 'be312a1' }
+        }
+      ]
+    },
+    {
+      id: 'lane2',
+      title: 'Completed',
+      label: '0/0',
+      cards: []
+    }
+  ]
+}
+
+const newData = [
+  {
+    id: 'lane1',
+    title: 'Planned Tasks',
+    label: '2/2',
+    cards: [
       {
-        id: 'lane1',
-        title: 'Planned Tasks',
-        label: '2/2',
-        cards: [
-          { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins' },
-          { id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
-        ]
-      },
+        id: 'Card1',
+        title: 'Nike',
+        description: 'Just do it',
+        label: '30 mins'
+      }
+    ]
+  },
+  {
+    id: 'lane2',
+    title: 'Completed',
+    label: '0/0',
+    cards: [
       {
-        id: 'lane2',
-        title: 'Completed',
-        label: '0/0',
-        cards: []
+        id: 'Card2',
+        title: 'Do something',
+        description: 'Do something something',
+        label: '45 mins'
       }
     ]
   }
-  return data
-}
-
+]
 
 const handleDragStart = (cardId: string, laneId: string) => {
   console.log('drag started')
@@ -32,7 +68,11 @@ const handleDragStart = (cardId: string, laneId: string) => {
   console.log(`laneId: ${laneId}`)
 }
 
-const handleDragEnd = (cardId: string, sourceLaneId: string, targetLaneId: string) => {
+const handleDragEnd = (
+  cardId: string,
+  sourceLaneId: string,
+  targetLaneId: string
+) => {
   console.log('drag ended')
   console.log(`cardId: ${cardId}`)
   console.log(`sourceLaneId: ${sourceLaneId}`)
@@ -43,7 +83,7 @@ export default class Area51 extends Component<any, any> {
   state = {
     boardData: {
       lanes: []
-    },
+    }
     // eventBus: null,
   }
 
@@ -51,24 +91,38 @@ export default class Area51 extends Component<any, any> {
     this.setState({ eventBus })
   }
 
-
-
-  
   addCard = () => {
     // @ts-ignore
-    this.state.eventBus.publish({ type: 'ADD_CARD', laneId: 'lane1', card: { id: "M1", title: "Buy Milk", label: "15 mins", description: "Also set reminder" } })
+    this.state.eventBus.publish({
+      type: 'ADD_CARD',
+      laneId: 'lane1',
+      card: {
+        id: 'M1',
+        title: 'Buy Milk',
+        label: '15 mins',
+        description: 'Also set reminder'
+      }
+    })
   }
 
   completeCard = () => {
     // @ts-ignore
     this.state.eventBus.publish({
-      type: 'MOVE_CARD', 
-      fromLaneId: 'lane1', 
-      toLaneId: 'lane2', 
-      cardId: 'M1', 
-      index: 0})
+      type: 'MOVE_CARD',
+      fromLaneId: 'lane1',
+      toLaneId: 'lane2',
+      cardId: 'M1',
+      index: 0
+    })
   }
 
+  updateData(newLaneData: any) {
+    // @ts-ignore
+    this.state.eventBus.publish({
+      type: 'UPDATE_LANES',
+      lanes: newLaneData
+    })
+  }
 
   handleCardAdd = (card: any, laneId: string) => {
     console.log(`New card added to lane ${laneId}`)
@@ -80,16 +134,13 @@ export default class Area51 extends Component<any, any> {
     console.log(nextData)
   }
 
-
-
   componentWillMount() {
-    const response = getBoard()
+    const response = data
     this.setState({ boardData: response })
-    setTimeout(this.addCard, 2000)
-    setTimeout(this.completeCard, 4000)
+    setTimeout(this.addCard, 1000)
+    setTimeout(this.completeCard, 2000)
+    setTimeout(this.updateData.bind(this, newData), 3000)
   }
-
-
 
   render() {
     return (
